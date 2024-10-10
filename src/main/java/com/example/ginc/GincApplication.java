bfd91534dfaf5802c805bbb54457135762b48e6d
jae9380 <ljy5314@gmail.com>
2024-10-10 10:47:52 +0900

    Created new project and commit

---
+++ src/main/java/com/example/ginc/GincApplication.java
@@ -0,0 +1,13 @@
+package com.example.ginc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GincApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GincApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ginc/GincApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ginc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GincApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
