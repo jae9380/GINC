6e9e8e2b0dfa94e927aed781489598cab7665584
jae9380 <ljy5314@gmail.com>
2024-11-18 22:28:16 +0900

    Design:

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -2,15 +2,23 @@
 	import '../tailwind.css';
 </script>
 
+
+
 <div class="flex flex-col min-h-screen">
 	<!-- 네비게이션 바 -->
-	<header class="bg-blue-500 text-white p-4 fixed top-0 left-0 w-full z-10">
-	  <nav class="flex items-center justify-between px-4 py-5 shadow-md">
-		<div class="flex items-center">
-		  <a href="/home" class="text-3xl">GINC</a>
+	<nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+		<div class="flex items-center flex-shrink-0 text-white mr-6">
+			<img src="https://i.postimg.cc/BvXN2Jg1/12345-removebg-preview.png" alt="GINC" class="h-8 w-8 mr-2" />
+		  <span class="font-semibold text-xl tracking-tight">GINC</span>
+		</div>
+		<div class="block fixed right-5">
+		  <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+			<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+		  </button>
 		</div>
 	  </nav>
-	</header>
+
+	
   
 	<!-- 메인 컨텐츠 -->
 	<main class="flex-1 mt-[80px] mb-[60px]">
